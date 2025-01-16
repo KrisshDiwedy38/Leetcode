f2ae894396496e6b0727e1498ce24e8c800ddcae
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-01-16 23:15:06 +0530

    Ques-Contains_Duplicate

---
+++ Contains_Duplicate-E.py
@@ -0,0 +1,11 @@
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+   visited = set()
+   for i in nums:
+      if i in visited:
+            return True
+      else:
+            visited.add(i)
+   return False
+
