7a8a7551725b5f968163834698f766a07200b4e0
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-02-01 22:41:53 +0530

    Updating

--- leetodePractice.py
+++ leetodePractice.py
@@ -1,9 +1,14 @@
-def twoSum( nums, target):
-   visited = []
-   for i in range(0,nums):
-      if target - nums[i] in visited:
-         
-      else:
-         visited.append(i)
+def plusOne(digits):
+   number = 0
+   for i in range(0,len(digits)):
+      number += digits[i] * (10 **( len(digits) - i - 1))
+   
+   number += 1 
+   new_digits = []
+   
+   while number != 0:
+      new_digits.append(number%10)
+      number //= 10
       
-print(twoSum([2,7,11,15],9))
+   return new_digits[::-1]
+
