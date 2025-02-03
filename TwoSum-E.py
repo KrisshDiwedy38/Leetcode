9676ef7075c56cdbfb083c720a63b89a80a0578d
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-02-03 22:56:03 +0530

    Optimal Solution

---
+++ TwoSum-E.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+   numbers = {}
+   
+   for i, value in enumerate(nums):
+      diff = target - value
+      if diff in numbers:
+         return [numbers[diff] , i]
+      numbers[value] = i
+      
