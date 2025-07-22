dbbca0d815dc725ea9282dd4194fe7aa658c348f
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-07-23 03:03:04 +0530

    Linked List

---
+++ Theory/Linked_Lists.py
@@ -0,0 +1,11 @@
+# Singly Linked List 
+
+class SinglyLL:
+   def __init__(self, val, next=None):
+      self.val = val
+      self.next = next
+
+   def __str__(self):
+      return str(self.val)
+   
+
