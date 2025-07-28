50b701d03fb27ca39afe4ae239be64e9078f2050
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-07-28 20:01:16 +0530

    Adjusting stuff

--- Theory/Linked_Lists.py
+++ Theory/Linked_Lists.py
@@ -1,6 +1,5 @@
-# Singly Linked List 
-
-class SinglyLL:
+# Singly Linked List Node
+class S_Node:
    def __init__(self, val, next=None):
       self.val = val
       self.next = next
@@ -8,4 +7,20 @@ class SinglyLL:
    def __str__(self):
       return str(self.val)
    
+# Singly Linked List Functions
 
+
+
+# Doubly Linked List Node
+class D_Node:
+   def __init__(self, val, next=None, prev=None):
+      self.val = val
+      self.next = next
+      self.prev = prev
+   
+   def __str__(self):
+      return str(self.val)
+   
+
+if __name__ == "__main__":
+   pass
