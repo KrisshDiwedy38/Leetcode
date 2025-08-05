0f0cb324f9c17234cf6f81d0ae1b9905e813f798
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-08-05 19:23:08 +0530

    Binary Search

--- Theory/Binary_Search.py
+++ Theory/Binary_Search.py
@@ -24,11 +24,61 @@ Advanced Binary Search Applications:
 
 
 class Binary_Search():
-    def __init__(self, arr):
-        self.arr = arr
-    
-    def standardBS(self,val):
-        pass
+    def __init__(self):
+        self.arr = []
 
-        
+    def get_array(self):
+        user_input = input("Enter a list of numbers seperated by space: ")
+        self.arr = list(map(int, user_input.split()))
+
+    def display_list(self):
+        result = " -> ".join(str(i) for i in self.arr)
+        print(result)
     
+    def iterative_BS(self):
+        target = int(input("Enter element to be searched: "))
+
+        left, right = 0, len(self.arr) - 1 
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if self.arr[mid] == target:
+                print(f"{target} found at {mid}")
+                return
+            elif self.arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        print("Element not found")
+        return -1 
+    
+    def recurr_BS(self, target = None, left = 0, right = None):
+        if right is None: 
+            target = int(input("Enter element to be searched: "))
+            right = len(self.arr) - 1
+
+        if left > right: 
+            return -1
+        
+        mid = left + (right - left) // 2
+
+        if self.arr[mid] == target:
+            print(f"{target} found at {mid}")
+            return
+        elif self.arr[mid] < target:
+            left = mid + 1
+            return self.recurr_BS(target, left, right)
+        else:
+            right = mid - 1
+            return self.recurr_BS(target, left , right)
+        
+
+if __name__ == "__main__":
+    arr = Binary_Search()
+    arr.get_array()
+    arr.display_list()
+    arr.iterative_BS()
+    arr.recurr_BS()
+
