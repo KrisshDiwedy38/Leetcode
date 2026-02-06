6069fdfeb94c5f1cae3ba05c681d023aad9dc5b9
Krissh Diwedy <krisshdiwedy38@gmail.com>
2026-02-06 10:09:33 +0530

    Editing

--- Questions/Vaild_Palindrome-E.py
+++ Questions/Vaild_Palindrome-E.py
@@ -1,8 +1,4 @@
 def isPalindrome(s):
-   # if s == "":
-   #    return True
-   # new_string = "".join(letters for letters in s if letters.isalnum())
-   # return new_string.lower() == new_string.lower()[::-1]
    s = s.replace(" ", "")
    if s == "":
       return True
